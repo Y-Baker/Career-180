5ab5d6272ac9ef0a5e13200e874bab426d861ae3
Y-Baker <yuossefahmedgaber@gmail.com>
2024-10-14 21:48:40 +0300

    Add Day 1 Task 2

--- Sessions/Day1/ConsoleApp/Program.cs
+++ Sessions/Day1/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StudentService;
+using TimeSpan;
 
 Console.WriteLine("Hello, World!");
 
@@ -8,3 +9,12 @@ student.AddScore(4, 3.7f);
 student.AddScore(3, 3.5f);
 
 Console.WriteLine(student.View());
+
+
+
+Duration D1 = new Duration(1, 10, 15);
+Console.WriteLine(D1.getstring());
+
+
+Duration D2 = new Duration(7800);
+Console.WriteLine(D2.getstring());
---
+++ Sessions/Day1/TimeSpan/Duration.cs
@@ -0,0 +1,29 @@
+﻿namespace TimeSpan;
+
+public class Duration
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+
+    public Duration(int hours, int minutes, int seconds)
+    {
+        Hours = hours;
+        Minutes = minutes;
+        Seconds = seconds;
+    }
+    public Duration(int seconds)
+    {
+        Hours = seconds / (60 * 60);
+        seconds -= Hours * (60 * 60);
+
+        Minutes = seconds / 60;
+        seconds -= Minutes * 60;
+
+        Seconds -= seconds;
+    }
+    public string getstring()
+    {
+        return $"Hours: {Hours}, Minutes :{Minutes} , Seconds :{Seconds}";
+    }
+}
