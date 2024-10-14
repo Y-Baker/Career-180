1620de6b2c2f24703fa4e7340a13ac1552df1fb7
Y-Baker <yuossefahmedgaber@gmail.com>
2024-10-14 21:33:42 +0300

    Add Task 1 Day 1

---
+++ Sessions/Day1/ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using StudentService;
+
+Console.WriteLine("Hello, World!");
+
+Student student = new() { Name = "Yousef", Email = "yuossefbakier@gmail.com" };
+
+student.AddScore(4, 3.7f);
+student.AddScore(3, 3.5f);
+
+Console.WriteLine(student.View());
---
+++ Sessions/Day1/StudentService/Student.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentService;
+
+public class Student
+{
+    public Guid Id { get; init; }
+    public required string Name { get; set; }
+    string? email;
+    public required string Email { 
+        get
+        {
+            return email ?? "";
+        }
+        set
+        {
+            if (new EmailAddressAttribute().IsValid(email))
+                email = value;
+            else
+                throw new Exception("Not Valid Email");
+        }}
+    public string? Mobile { get; set; }
+    public int Age { get; set; }
+    public int TotalCreditHours { get; set; }
+    float gpa;
+    public float GPA
+    {
+        get => gpa;
+        set
+        {
+            if (value < 0 || value > 4)
+                throw new Exception("GPA must be between 0 and 4");
+            gpa = value;
+        }
+    }
+
+    public Student()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Student(string name, string email, string mobile, int age, int creditHours, float gpa)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        Mobile = mobile;
+        Age = age;
+        TotalCreditHours = creditHours;
+        GPA = gpa;
+    }
+
+    public void AddScore(int courseHours, float grade)
+    {
+        if (grade < 0 || grade > 4)
+            throw new Exception("Grade must be between 0 and 4");
+        float points = GPA * TotalCreditHours;
+        float newPoints = grade * courseHours;
+
+        TotalCreditHours += courseHours;
+        GPA = (points + newPoints) / TotalCreditHours;
+    }
+
+    public string View()
+    {
+        return $"{Id} {Name} {Email} {Mobile} {Age} {TotalCreditHours} {GPA}";
+    }
+}
