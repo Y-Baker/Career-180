fd9ce2fc3f94a298faee9b67377cb059ceb07292
Y-Baker <yuossefahmedgaber@gmail.com>
2024-10-16 21:09:27 +0300

    Task Day 3

---
+++ Sessions/Day3/ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using TimeSpan;
+
+Duration D1 = new Duration(1, 10, 15);
+Console.WriteLine(D1);
+
+
+Duration D2 = new Duration(7800);
+Console.WriteLine(D2);
+
+Duration D3 = new Duration(2, 10, 0);
+
+if (!D1.Equals(D2))
+{
+    Console.WriteLine("D1 and D2 are not equal");
+}
+
+if (D2.Equals(D3))
+{
+    Console.WriteLine("D2 and D3 are equal");
+}
+
+D3 = D1 + D2;
+Console.WriteLine(D3);
+
+D3 = 666 + D3;
+Console.WriteLine(D3);
+
+D3 = D1++;
+Console.WriteLine(D3);
+
+D3 = --D2;
+Console.WriteLine(D3);
+Console.WriteLine(D2);
+
+if (D1 <= D2)
+{
+    Console.WriteLine("D1 is less than or equal to D2");
+}
+else
+{
+    Console.WriteLine("D1 is greater than D2");
+}
---
+++ Sessions/Day3/TimeSpan/Duration.cs
@@ -0,0 +1,111 @@
+﻿namespace TimeSpan;
+
+public class Duration
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+
+    public Duration(int hours, int minutes, int seconds)
+    {
+        Hours = hours;
+        Minutes = minutes;
+        Seconds = seconds;
+    }
+    public Duration(int seconds)
+    {
+        Hours = seconds / (60 * 60);
+        seconds -= Hours * (60 * 60);
+
+        Minutes = seconds / 60;
+        seconds -= Minutes * 60;
+
+        Seconds -= seconds;
+    }
+    public string getstring()
+    {
+        return $"Hours: {Hours}, Minutes :{Minutes} , Seconds :{Seconds}";
+    }
+
+    public override string ToString()
+    {
+        // 11H:12M:25S
+        return $"{Hours}H:{Minutes}M:{Seconds}S";
+    }
+
+    public int TotalSeconds()
+    {
+        return Hours * 60 * 60 + Minutes * 60 + Seconds;
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is Duration duration)
+        {
+            return Hours == duration.Hours && Minutes == duration.Minutes && Seconds == duration.Seconds;
+        }
+        return false;
+    }
+
+    public static Duration operator +(Duration duration1, Duration duration2)
+    {
+        int seconds, minutes = 0, hours = 0;
+        seconds = duration1.Seconds + duration2.Seconds;
+        if (seconds >= 60)
+        {
+            seconds -= 60;
+            minutes++;
+        }
+        minutes += duration1.Minutes + duration2.Minutes;
+        if (minutes >= 60)
+        {
+            minutes -= 60;
+            hours++;
+        }
+        hours += duration1.Hours + duration2.Hours;
+        return new Duration(hours, minutes, seconds);
+    }
+
+    public static Duration operator +(int seconds, Duration duration1)
+    {
+        Duration duration2 = new Duration(seconds);
+        return duration1 + duration2;
+    }
+
+    public static Duration operator ++(Duration duration)
+    {
+        Duration tmp = new Duration(60);
+        return duration + tmp;
+    }
+
+    public static Duration operator --(Duration duration)
+    {
+        if (duration.Minutes > 0)
+        {
+            duration.Minutes--;
+            return duration;
+        }
+        if (duration.Hours > 0 && duration.Minutes == 0)
+        {
+            duration.Hours--;
+            duration.Minutes = 59;
+            return duration;
+        }
+        throw new Exception("Duration cannot be negative");
+    }
+
+    public static bool operator <=(Duration duration1, Duration duration2)
+    {
+        int totalSeconds1 = duration1.TotalSeconds();
+        int totalSeconds2 = duration2.TotalSeconds();
+
+        return totalSeconds1 <= totalSeconds2;
+    }
+
+    public static bool operator >=(Duration duration1, Duration duration2)
+    {
+        int totalSeconds1 = duration1.TotalSeconds();
+        int totalSeconds2 = duration2.TotalSeconds();
+
+        return totalSeconds1 >= totalSeconds2;
+    }
+}
