39854c6f4b9387c868faf6334c9b685b551f263f
Y-Baker <yuossefahmedgaber@gmail.com>
2024-10-19 19:15:44 +0300

    Day4 Task1

---
+++ Sessions/Day4/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSA;
+
+Console.WriteLine("Hello, World!");
+
+_Queue<int> q = new(5);
+Console.WriteLine(q.Pop());
+q.Push(1);
+q.Push(2);
+q.Push(3);
+q.Push(4);
+q.Push(5);
+
+Console.WriteLine(q.Pop());
+Console.WriteLine(q.Pop());
+Console.WriteLine(q.Pop());
+Console.WriteLine(q.Pop());
+Console.WriteLine(q.Pop());
+Console.WriteLine(q.Pop());
+
---
+++ Sessions/Day4/DSA/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace DSA;
+
+public class _Queue<T>
+{
+    T[] _arr;
+    int rear;
+    const int _front = 0;
+
+    public _Queue(int size = 10)
+    {
+        _arr = new T[size];
+        rear = 0;
+    }
+
+    public void Push(T item)
+    {
+        if (rear == _arr.Length)
+            throw new Exception("Queue is full");
+        _arr[rear++] = item;
+    }
+
+    public T? Pop()
+    {
+
+        if (rear == 0)
+            return default(T);
+        
+        T re = _arr[_front];
+
+        for (int i = 0; i < rear - 1; i++)
+        {
+            _arr[i] = _arr[i + 1];
+        }
+
+        rear--;
+        return re;
+    }
+}
