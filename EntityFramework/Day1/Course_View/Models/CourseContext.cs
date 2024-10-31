45a93d3ea6ff2e2335e71f3c2fc2a0c2101925de
Y-Baker <yuossefahmedgaber@gmail.com>
2024-10-31 15:05:22 +0300

    Fix Security Issue

--- EntityFramework/Day1/Course_View/Models/CourseContext.cs
+++ EntityFramework/Day1/Course_View/Models/CourseContext.cs
@@ -23,7 +23,7 @@ public partial class CourseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=tatakai.database.windows.net;Initial Catalog=StudentsAffairsDB;Persist Security Info=True;User ID=YAhmed;Password=Yousef$3114");
+        => optionsBuilder.UseSqlServer("Data Source=tatakai.database.windows.net;Initial Catalog=StudentsAffairsDB;Persist Security Info=True;User ID=?;Password=?");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
